71d30cc3501691b05839865fefbdd28f3a1b63a4
espern <loic.espern@gmail.com>
2020-12-28 16:25:25 +0100

    class vs interface

---
+++ src/class-vs-interface.ts
@@ -0,0 +1,26 @@
+type Article = {
+  title: string,
+  price: number,
+  vat: number,
+  stock?: number,
+  description?: string
+}
+
+interface ShopItem {
+  title: string
+  price: number
+  vat: number
+  stock?: number
+  description?: string
+}
+
+// declaration merging here...
+interface ShopItem {
+  reviews: {
+    rating: number,
+    content: string
+  }[]
+}
+
+let article: Article = undefined
+let shopItem: ShopItem = undefined
